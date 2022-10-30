6efa784365c2cac3feccf2c41106e8ef759f2edb
choihyeongjun <69442251+choihyeongjun@users.noreply.github.com>
2022-10-30 20:47:36 +0900

    feature/eurekaservercommit

---
+++ src/test/java/com/example/theparaboleinfra/TheParaboleInfraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.theparaboleinfra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheParaboleInfraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
